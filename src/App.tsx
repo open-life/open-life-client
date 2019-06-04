f8d881413d5173d352b4a9bf1064c8eb6f0689e6
Phillip Chaffee <phillipdensmorechaffee@gmail.com>
2019-06-04 06:49:47 -0400

    Basic app setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="container">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">
+              Open Life!
+      </h1>
+            <h2 className="subtitle">
+              Hero subtitle
+      </h2>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
