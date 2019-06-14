989f6f698e44a0fbca60ee726f6b4c9d96c934ad
Phillip Chaffee <phillipdensmorechaffee@gmail.com>
2019-06-14 06:53:03 -0400

    Basic title bar & start on goals overview.

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -9,9 +9,20 @@ const Profile: React.FC = () => {
           <figure className="image avatar">
             <img className="is-rounded avatar-image" src="/profile.jpg" />
           </figure>
-          <h2 className="title is-2 has-text-white">Phillip Chaffee</h2>
+          <div className="name-stats">
+            <h2 className="is-size-2 has-text-white name">Phillip Chaffee</h2>
+            <div className="stats">
+              <h2 className="is-size-2 has-text-white goals">8 goals</h2>
+            </div>
+          </div>
         </section>
       </header>
+
+      <section className="section">
+        <div className="container has-text-centered">
+          <h2 className="title is-2">2019 Goals</h2>
+        </div>
+      </section>
     </div>
   );
 }
