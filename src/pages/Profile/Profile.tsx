087d1e5ad32f2942dfa61c6ec2f99f2cb4b25446
Phillip Chaffee <phillipdensmorechaffee@gmail.com>
2019-06-28 06:53:06 -0400

    Number goal graph.

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -19,11 +19,19 @@ export default class Profile extends React.Component<Props, State> {
         y += 23;
       }
 
-      var rect = draw.rect(21, 21).attr({ x: x, y: y });
+      draw.rect(21, 21).attr({ x: x, y: y });
       x = x + 23;
     }
 
     draw.height(y + 23);
+  }
+
+  rand(min: number, max: number) {
+    var seed = 7;
+    min = min === undefined ? 0 : min;
+    max = max === undefined ? 1 : max;
+    seed = (seed * 9301 + 49297) % 233280;
+    return min + (seed / 233280) * (max - min);
   }
 
   componentDidMount() {
@@ -135,6 +143,47 @@ export default class Profile extends React.Component<Props, State> {
               </div>
             </div>
             <div className="box">
+              <h5 className="title is-5">Monthly Recurring Revenue</h5>
+              <LineChart data={{
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                datasets: [{
+                  label: 'Filled',
+                  backgroundColor: '#FFFFFF',
+                  borderColor: '#FFFFFF',
+                  data: [0, 100, 250, 0, 425, 310, 200],
+                  fill: true
+                }]
+              }} options={{
+                responsive: true,
+                title: {
+                  display: true,
+                  text: 'Chart.js Line Chart'
+                },
+                tooltips: {
+                  mode: 'index',
+                  intersect: false,
+                },
+                hover: {
+                  mode: 'nearest',
+                  intersect: true
+                },
+                scales: {
+                  xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Month'
+                    }
+                  }],
+                  yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Value'
+                    }
+                  }]
+                }
+              }} width="600" height="250" />
             </div>
           </div>
         </section>
