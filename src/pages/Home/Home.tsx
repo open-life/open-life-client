0d9ebe121821e8bf2fac198dd8e0b4cfc04e1232
Phillip Chaffee <phillipdensmorechaffee@gmail.com>
2019-07-03 06:26:40 -0400

    Get routing working.

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Home.css';
+
+interface Props { };
+interface State { };
+
+export default class Home extends React.Component<Props, State> {
+    render() {
+        return <h1>Hello</h1>;
+    }
+}
